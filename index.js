4c1ab09a2d49b314fd2772f97142e8307bf123a0
azul-de-prusia <tegthyn_600@hotmail.com>
2019-08-19 16:15:57 -0500

    first test, install Eslint

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-module.exports = () => {
-  // ...
+const path = require('path');
+
+module.exports = (filePath) => {
+if (path.extname(filePath)==='.md'){
+  return true;
+} else {
+  return false;
+}
+ //ternario, síntesis del if anterior
+ //return path.extname(filePath)==='.md' ? true : false;
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -3,8 +3,12 @@ const mdLinks = require('../');
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should return true for a valid .md file', () => {
+    expect(mdLinks('.../README.md')).toBe(true);
+  });
+
+  it('should return true for an invalid .md file', () => {
+    expect(mdLinks('.../index.js')).toBe(false);
   });
 
 });
